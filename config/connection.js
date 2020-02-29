aeecc1187fa3b29277fb30919688c371728e6cd9
elee0888 <e.lee@my.ccsu.edu>
2020-02-29 14:20:27 -0500

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
 //setting connection to mysql and database//
 var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 connection = mysql.createConnection({
     host:"localhost",
     user: "root",
     password: "blasT123!",
     database: "burgers_db"
 });
+};
 //connecting to mysql and database//
 connection.connect(function(err){
     if (err){
